14507b6f9d9e8b3d859d4b40a7fdef9ba81b43a5
Mr.Hu <mr.hulei@qq.com>
2022-10-15 20:15:08 +0800

    学习SpringBoot基础

--- src/main/java/com/hulei/myspringboot/MyspringbootApplication.java
+++ src/main/java/com/hulei/myspringboot/MyspringbootApplication.java
@@ -2,12 +2,24 @@ package com.hulei.myspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * 主程序类-
+ * @SpringBootApplication:这是一个SpringBoot应用
+ */
 
 @SpringBootApplication
 public class MyspringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyspringbootApplication.class, args);
+//        返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MyspringbootApplication.class, args);
+//        查看容器里面的组件
+        String names[] = run.getBeanDefinitionNames();
+        for(String name : names){
+            System.out.println("name = " + name);
+        }
     }
 
 }
